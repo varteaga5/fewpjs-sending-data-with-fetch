75cc69d145e1b2e87638e1832c857417deeded5e
vincent <vincent.arteaga@gmail.com>
2021-04-23 13:30:09 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+    const formData = {
+        'name': name,
+        'email': email
+    };
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch('http://localhost:3000/users', configObj)
+    .then(resp => resp.json())
+    .then(obj => renderID(obj))
+    .catch(renderError)
+}
+
+// appends ID to DOM
+function renderID(obj){
+    // displays id on the body of the DOM
+    document.body.innerHTML = obj.id;
+}
+// appends error msg to DOM
+function renderError(obj) {
+    document.body.innerHTML = obj.message
+}
